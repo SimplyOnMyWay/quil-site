4d65655a82b11315cd2965390c818ba54c07dc82
SimplyOnMyWay <micheal.ocathain@gmail.com>
2023-03-06 17:50:12 +0000

    photobook.clj update

--- src/clj/quil_site/views/photobook.clj
+++ src/clj/quil_site/views/photobook.clj
@@ -5,7 +5,7 @@
 (defn photobook-page []
   (page {:tab :photobook
          :type :photobook-page
-         :js-files ["/js/main.js"]
+         :js-files [""]
          :title "Photobook on Irish Digital Art - Mícheál Ó Catháin"}
 
         [:div.section
@@ -33,7 +33,7 @@
                [:li {} "Music, installation, performance"]
                [:li {} "Engaging with digital archive"]
                [:li {} "Harp"]
-               [:li {} "GAC exhib - luthchleasa meaning"]
+               [:li {} "GAC exhib - luthchleas"]
                [:li {} "What ideas Fearn represenation"]
                [:li {} "Size of Bunting notebook"]]
 
@@ -55,9 +55,124 @@
               [:br]
 
               ]
-             
-             
 
-]]]]]))
+             [:div {:class "column" :id "hi-res"}
+              [:h3 {} "Fearn generative art"]
+
+
+              [:p {} [:strong {} "Digital archival prints on Hahnemühle Museum Etching paper, with straight edge"]]
+              [:p {}  "Frame visualisation is indicative only. Dimensions and prices unframed. Click on each thumbnail for a higher resolution image of the artwork."]
+              [:p {} "The prints for the Fearn series have dimensions 141 x 121 mm, identical to the paper Bunting used in his field collecting of the old music from Irish harpers in the period 1792 - ca. 1805."]
+              [:p {} "Four " [:em "lúthchleasa"] " are represented in the Fearn series:"]
+              [:ul
+               [:li {} [:em "Leagadh anuas"] " , translated from Gaelic to English as \"falling/knocked down from above\". Four prints."]
+               [:li {} [:em "Leath leagadh"] " , translated as \"half-falling\". Three prints."]
+               [:li {} [:em "Brisidh"] " , translated as 'a break'. Six prints"]
+               [:li {} [:em "Crothach aon mhéar"] " , translated as 'one finger shaking'. Two prints"]]
+              [:br]
+              [:figure {:class "hormarginstight" :style "width:10%;display:inline-block;"}
+               [:a {:href "../img/art/luthchleasa_documentation/hi-res/fearn-01--20211123T233014518-seed-3780__5640x4840_GAC-FINAL.tif"}
+                [:img {:class "hormarginstight" :src "../img/art/luthchleasa/fearn-01-thumbnail-framed.png" :width "100%"}]]
+               [:figcaption {:style "text-align:center;height:80px"}
+                [:span {:style "font-size:12px"} [:em "Leagadh anuas I"]]
+                [:span {:style "font-size:9px"} [:br] "141x121mm" [:br] "2021" [:br] "1/1" [:br] "€95 + P&P"]]]
+              [:figure {:class "hormarginstight" :style "width:10%;display:inline-block;"}
+               [:a {:href "../img/art/luthchleasa_documentation/hi-res/fearn-02--20211123T231136929-seed-5870__5640x4840_GAC-FINAL.tif"}
+                [:img {:class "hormarginstight" :src "../img/art/luthchleasa/fearn-02-thumbnail-framed.png" :width "100%"}]]
+               [:figcaption {:style "text-align:center"}
+                [:span {:style "font-size:12px"} [:em "Leagadh anuas II"]]
+                [:span {:style "font-size:9px"} [:br] "141x121mm" [:br] "2021" [:br] "1/1" [:br] "€95 + P&P"]]]
+              [:figure {:class "hormarginstight" :style "width:10%;display:inline-block;"}
+               [:a {:href "../img/art/luthchleasa_documentation/hi-res/fearn-03--20211123T231205258-seed-8610__5640x4840_GAC-FINAL.tif"}
+                [:img {:class "hormarginstight" :src "../img/art/luthchleasa/fearn-03-thumbnail-framed.png" :width "100%"}]]
+               [:figcaption {:style "text-align:center"}
+                [:span {:style "font-size:12px"} [:em "Leath leagadh I"]]
+                [:span {:style "font-size:9px"} [:br] "141x121mm" [:br] "2021" [:br] "1/1" [:br] "€95 + P&P"]]]
+              [:figure {:class "hormarginstight" :style "width:10%;display:inline-block;"}
+               [:a {:href "../img/art/luthchleasa_documentation/hi-res/fearn-05--20211123T23363162-seed-6280__5640x4840_GAC-FINAL.tif"}
+                [:img {:class "hormarginstight" :src "../img/art/luthchleasa/fearn-04-thumbnail-framed.png" :width "100%"}]]
+               [:figcaption {:style "text-align:center"}
+                [:span {:style "font-size:12px"} [:em "Leagadh anuas III"]]
+                [:span {:style "font-size:9px"} [:br] "141x121mm" [:br] "2021" [:br] "1/1" [:br] "€95 + P&P"]]]
+              [:figure {:class "hormarginstight" :style "width:10%;display:inline-block;"}
+               [:a {:href "../img/art/luthchleasa_documentation/hi-res/fearn-05--20211123T23363162-seed-6280__5640x4840_GAC-FINAL.tif"}
+                [:img {:class "hormarginstight" :src "../img/art/luthchleasa/fearn-05-thumbnail-framed.png" :width "100%"}]]
+               [:figcaption {:style "text-align:center"}
+                [:span {:style "font-size:12px"} [:em "Leath leagadh II"]]
+                [:span {:style "font-size:9px"} [:br] "141x121mm" [:br] "2021" [:br] "1/1" [:br] "€95 + P&P"]]]
+              [:figure {:class "hormarginstight" :style "width:10%;display:inline-block;"}
+               [:a {:href "../img/art/luthchleasa_documentation/hi-res/fearn-06--20211123T231211510-seed-3310__5640x4840_GAC-FINAL.tif"}
+                [:img {:class "hormarginstight" :src "../img/art/luthchleasa/fearn-06-thumbnail-framed.png" :width "100%"}]]
+               [:figcaption {:style "text-align:center"}
+                [:span {:style "font-size:12px"} [:em "Brisidh I"]]
+                [:span {:style "font-size:9px"} [:br] "141x121m" [:br] "2021" [:br] "1/1" [:br] "€95 + P&P"]]]
+              [:figure {:class "hormarginstight" :style "width:10%;display:inline-block;"}
+               [:a {:href "../img/art/luthchleasa_documentation/hi-res/fearn-07--20211123T231219592-seed-3100__5640x4840_GAC-FINAL.tif"}
+                [:img {:class "hormarginstight" :src "../img/art/luthchleasa/fearn-07-thumbnail-framed.png" :width "100%"}]]
+               [:figcaption {:style "text-align:center"}
+                [:span {:style "font-size:12px"} [:em "Brisidh II"]]
+                [:span {:style "font-size:9px"} [:br] "141x121mm" [:br] "2021" [:br] "1/1" [:br] "€95 + P&P"]]]
+
+              [:br]
+              [:br]
+              
+              [:figure {:class "hormarginstight" :style "width:10%;display:inline-block;"}
+               [:a {:href "../img/art/luthchleasa_documentation/hi-res/fearn-08--20211123T231328490-seed-3760__5640x4840_GAC-FINAL.tif"}
+                [:img {:class "hormarginstight" :src "../img/art/luthchleasa/fearn-08-thumbnail-framed.png" :width "100%"}]]
+               [:figcaption {:style "text-align:center"}
+                [:span {:style "font-size:12px"} [:em "Crothach aon mhéar I"]]
+                [:span {:style "font-size:9px"} [:br] "141x121mm" [:br] "2021" [:br] "1/1" [:br] "€95 + P&P"]]]
+              [:figure {:class "hormarginstight" :style "width:10%;display:inline-block;"}
+               [:a {:href "../img/art/luthchleasa_documentation/hi-res/fearn-09--20211123T231400875-seed-10__5640x4840_GAC-FINAL.tif"}
+                [:img {:class "hormarginstight" :src "../img/art/luthchleasa/fearn-09-thumbnail-framed.png" :width "100%"}]]
+               [:figcaption {:style "text-align:center"}
+                [:span {:style "font-size:12px"} [:em "Leagadh anuas IV"]]
+                [:span {:style "font-size:9px"} [:br] "141x121mm" [:br] "2021" [:br] "1/1" [:br] "€95 + P&P"]]]
+              [:figure {:class "hormarginstight" :style "width:10%;display:inline-block;"}
+               [:a {:href "../img/art/luthchleasa_documentation/hi-res/fearn-10--20211123T231410615-seed-2830__5640x4840_GAC-FINAL.tif"}
+                [:img {:class "hormarginstight" :src "../img/art/luthchleasa/fearn-10-thumbnail-framed.png" :width "100%"}]]
+               [:figcaption {:style "text-align:center"}
+                [:span {:style "font-size:12px"} [:em "Brisidh III"]]
+                [:span {:style "font-size:9px"} [:br] "141x121mm" [:br] "2021" [:br] "1/1" [:br] "€95 + P&P"]]]
+              [:figure {:class "hormarginstight" :style "width:10%;display:inline-block;"}
+               [:a {:href "../img/art/luthchleasa_documentation/hi-res/fearn-11--20211123T23115222-seed-7440__5640x4840_GAC-FINAL.tif"}
+                [:img {:class "hormarginstight" :src "../img/art/luthchleasa/fearn-11-thumbnail-framed.png" :width "100%"}]]
+               [:figcaption {:style "text-align:center"}
+                [:span {:style "font-size:12px"} [:em "Brisidh IV"]]
+                [:span {:style "font-size:9px"} [:br] "141x121mm" [:br] "2021" [:br] "1/1" [:br] "€95 + P&P"]]]
+              [:figure {:class "hormarginstight" :style "width:10%;display:inline-block;"}
+               [:a {:href "../img/art/luthchleasa_documentation/hi-res/fearn-12--20211123T231423670-seed-6590__5640x4840_GAC-FINAL.tif"}
+                [:img {:class "hormarginstight" :src "../img/art/luthchleasa/fearn-12-thumbnail-framed.png" :width "100%"}]]
+               [:figcaption {:style "text-align:center"}
+                [:span {:style "font-size:12px"} [:em "Leath leagadh III"]]
+                [:span {:style "font-size:9px"} [:br] "141x121mm" [:br] "2021" [:br] "1/1" [:br] "€95 + P&P"]]]
+              [:figure {:class "hormarginstight" :style "width:10%;display:inline-block;"}
+               [:a {:href "../img/art/luthchleasa_documentation/hi-res/fearn-13--20211123T231442414-seed-7370__5640x4840_GAC-FINAL.tif"}
+                [:img {:class "hormarginstight" :src "../img/art/luthchleasa/fearn-13-thumbnail-framed.png" :width "100%"}]]
+               [:figcaption {:style "text-align:center"}
+                [:span {:style "font-size:12px"} [:em "Crothach aon mhéar II"]]
+                [:span {:style "font-size:9px"} [:br] "141x121mm" [:br] "2021" [:br] "1/1" [:br] "€95 + P&P"]]]
+              [:figure {:class "hormarginstight" :style "width:10%;display:inline-block;"}
+               [:a {:href "../img/art/luthchleasa_documentation/hi-res/fearn-14--20211123T231508849-seed-8750__5640x4840_GAC-FINAL.tif"}
+                [:img {:class "hormarginstight" :src "../img/art/luthchleasa/fearn-14-thumbnail-framed.png" :width "100%"}]]
+               [:figcaption {:style "text-align:center"}
+                [:span {:style "font-size:13px"} [:em "Brisidh V"]]
+                [:span {:style "font-size:9px"} [:br] "141x121mm" [:br] "2021" [:br] "1/1" [:br] "€95 + P&P"]]]
+              [:figure {:class "hormarginstight" :style "width:10%;display:inline-block;"}
+               [:a {:href "../img/art/luthchleasa_documentation/hi-res/fearn-15--20211123T233753372-seed-3610__5640x4840_GAC-FINAL.tif"}
+                [:img {:class "hormarginstight" :src "../img/art/luthchleasa/fearn-15-thumbnail-framed.png" :width "100%"}]]
+               [:figcaption {:style "text-align:center"}
+                [:span {:style "font-size:13px"} [:em "Brisidh VI"]]
+                [:span {:style "font-size:9px"} [:br] "141x121mm" [:br] "2021" [:br] "1/1" [:br] "€95 + P&P"]]]              
+              [:br]
+              [:br]
+
+
+              
+              
+              ]
+             
+             ]]]]]
+        ))
 
 
